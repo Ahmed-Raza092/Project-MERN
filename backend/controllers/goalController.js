7648c0c788e31a55a147ae47a655fad2c827c8ed
Ahmed-Raza092 <m.raza9256@gmail.com>
2023-06-02 00:47:25 +0500

    2nd Milestone

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,5 +1,6 @@
 const asyncHandler = require('express-async-handler')
 const Goal = require('../models/goalModel')
+const User = require('../models/userModel')
 
 
 //@desc Get goals
@@ -7,7 +8,8 @@ const Goal = require('../models/goalModel')
 //@access Private
 
 const getGoals = asyncHandler( async (req, res)=>{ 
-    const goals = await Goal.find()
+    const goals = await Goal.find({ user: req.user.id })
+
 
     res.status(200).json(goals)
 })
@@ -20,7 +22,8 @@ const setGoal = asyncHandler( async (req, res)=>{
     }
 
     const goal = await Goal.create({
-        text: req.body.text
+        text: req.body.text,
+        user: req.user.id,
     })
    
     res.status(200).json(goal)
@@ -33,6 +36,22 @@ const updateGoal = asyncHandler( async (req, res)=>{
     if(!goal){
         res.status(400)
         throw new Error('Goal not found')
+    }
+
+    const user = await User.findById(req.user.id)
+
+    // Check for User
+    if(!user){
+        res.status(401)
+        throw new Error('User not found')
+    }
+
+
+    // make sure the logged in user matches the goal user
+    if(goal.user.toString() !== user.id){
+        res.status(401)
+        throw new Error('User not Authorized')
+
     }
 
     const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {new: true})
@@ -50,6 +69,22 @@ const deleteGoal = asyncHandler( async (req, res)=>{
         throw new Error('Goal not found')
     }else{
         await Goal.findByIdAndDelete(req.params.id)
+    }
+
+    const user = await User.findById(req.user.id)
+
+    // Check for User
+    if(!user){
+        res.status(401)
+        throw new Error('User not found')
+    }
+
+
+    // make sure the logged in user matches the goal user
+    if(goal.user.toString() !== user.id){
+        res.status(401)
+        throw new Error('User not Authorized')
+
     }
 
    
