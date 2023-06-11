018d8f92afe14775c30d66478ad6d12b9bdb7298
Ahmed-Raza092 <m.raza9256@gmail.com>
2023-06-11 17:05:15 +0500

    3rd Milestone

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,12 @@
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
+import { ToastContainer } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css'
 import Dashboard from "./pages/Dashboard";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Header from "./components/Header";
+
+
 
 function App() {
   return (
@@ -18,6 +22,7 @@ function App() {
       
     </div>
     </Router>
+    <ToastContainer />
     </>
   );
 }
